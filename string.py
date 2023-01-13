a34c0d88b16842105a582a75035158e9ea117747
SheraliJurayev <100849791+SheraliJurayev@users.noreply.github.com>
2023-01-13 13:00:19 +0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 21:30:12 2023
+
+@author: Sh_Jurayeff
+"""
+a = int(input("Yosh: "))
+if a<4 or a>60:
+    print("Sizlar uchun beletlarimiz tekin.")
+elif a<=18:
+    print("Sizdan 10000 so'm ")
+elif a>18:
+    print("Sizdan 20000 so'm")
+    
+    
+        
+        
+
+
